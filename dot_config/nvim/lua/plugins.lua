b77a9631f04109c7019e0a16d203879f30b2d54e
tuannvm <tuannvm@hotmail.com>
2021-08-10 23:53:58 +0800

    Update config

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,4 +1,4 @@
--- This file can be loaded by calling `lua require('plugins'` from your init.vim
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 return require('packer').startup(function()
 
@@ -9,26 +9,35 @@ return require('packer').startup(function()
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use 'hrsh7th/vim-vsnip'
   use 'hrsh7th/vim-vsnip-integ'
+
 -- editorconfig
   use 'editorconfig/editorconfig-vim'
+
 -- completion
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/nvim-compe'
+
 -- git support
   use 'iberianpig/tig-explorer.vim'
   use 'rbgrouleff/bclose.vim'
+
 -- terraform support
   use 'hashivim/vim-terraform'
+
 -- theme
   use 'marko-cerovac/material.nvim'
+
 -- tmux
 use 'numToStr/Navigator.nvim'
+
 -- fuzzy search
 use 'nvim-lua/popup.nvim'
 use 'nvim-lua/plenary.nvim'
 use 'nvim-telescope/telescope.nvim'
+
 -- statusbar
   use 'hoob3rt/lualine.nvim'
+
 -- file explorer
   use 'kyazdani42/nvim-tree.lua'
   use 'kyazdani42/nvim-web-devicons'
--- dot_oh-my-zsh/custom/private_themes/tommy.zsh-theme
+++ dot_oh-my-zsh/custom/private_themes/tommy.zsh-theme
@@ -1,7 +1,7 @@
-local ret_status="%(?:%{$fg_bold[green]%}λ:%{$fg_bold[green]%}λ)"
-PROMPT="${ret_status} %{$fg[cyan]%}%c%{$reset_color%} %# "
+local ret_status="%(?:%{$fg_bold[magenta]%}λ:%{$fg_bold[magenta]%}λ)"
+PROMPT="${ret_status} %{$fg[yellow]%}%c%{$reset_color%} %# "
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[green]%}(%{$fg[blue]%}"
+ZSH_THEME_GIT_PROMPT_PREFIX=""
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[green]%}) %{$fg[yellow]%}✗"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}) %{$fg[yellow]%}✗"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%})"
