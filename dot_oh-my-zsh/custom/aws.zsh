9122a39aaf7780dd9ea1fe92496bdce593f45921
tuannvm <me@tuannvm.com>
2022-10-14 10:48:12 +0800

    Update

---
+++ dot_oh-my-zsh/custom/aws.zsh
@@ -0,0 +1,8 @@
+function ec2ips() {
+  aws ec2 describe-instances --filters "Name=instance-id ,Values=*$1*" --query "Reservations[].Instances[].NetworkInterfaces[].[Association.PublicIp,PrivateIpAddress]"
+}
+
+function ec2ssh () {
+  arr=(`echo $3 | tr '@' ' '`)
+  ssh $1 $2 ${arr[1]}@$(aws ec2 describe-instances --filter Name=instance-id,Values=${arr[2]} | jq '.Reservations[0].Instances[0].PrivateIpAddress' | tr -d '"')
+}
